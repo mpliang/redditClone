75d93ea780bc27bc4778cf77b7d6c6ee1d4d57bc
Michael Liang <michaelpliang@gmail.com>
2015-11-11 16:38:07 -0800

    updated schemas

---
+++ src/js/app/controllers/MapCtrl_REMOTE_67684.js
@@ -0,0 +1,9 @@
+var app = angular.module('redditApp');
+
+
+
+app.controller('MapCtrl', function($scope, $ionicLoading, $compile, landmark, $ionicModal, $rootScope, auth) {
+  auth.getCurrentUserInfo()
+
+
+});
